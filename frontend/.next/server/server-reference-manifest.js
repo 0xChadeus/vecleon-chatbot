f2884e0d6ce3d7b7233df80f3dd01a2531465efd
saze@protonmail.com <saze@protonmail.com>
2023-12-22 14:30:12 +1100

    initial commit

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OyjAK1JTGP+4y0NL1BE9qUMZRcSwuwBB3EE7OflH2No=\"\n}"
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { LandingNavbar } from '@/components/landing-navbar'
+
+//home (landing) page
+export default function Home() {
+  return (
+    <>
+      <LandingNavbar/>
+    </>
+  )
+}
